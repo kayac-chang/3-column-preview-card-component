2a4b4df401e16da2b593fc4cb501a67ea6a46821
kayac-chang <egg734631@gmail.com>
2022-03-14 00:10:07 +0800

    feat: 🎸 final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as IconLuxury } from "./assets/luxury.svg";
+import { ReactComponent as IconSedans } from "./assets/sedans.svg";
+import { ReactComponent as IconSuvs } from "./assets/suvs.svg";
+
+function Card({ className, icon, title, content }) {
+  return (
+    <div className={"card " + className}>
+      <header>
+        {icon}
+
+        <strong>{title}</strong>
+      </header>
+
+      <p>{content}</p>
+
+      <button>Learn More</button>
+    </div>
+  );
+}
 
 function App() {
+  const cards = [
+    {
+      icon: <IconSedans />,
+      title: "Sedans",
+      content: `Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.`,
+      className: "orange",
+    },
+    {
+      icon: <IconSuvs />,
+      title: "SUVs",
+      content: `Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.`,
+      className: "blue",
+    },
+    {
+      icon: <IconLuxury />,
+      title: "Luxury",
+      content: `Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.`,
+      className: "green",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {cards.map((card) => (
+        <Card key={card.title} {...card} />
+      ))}
     </div>
   );
 }
